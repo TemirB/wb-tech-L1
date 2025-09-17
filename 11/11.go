7c1c57cb0476e9b8a1b7d2ad7adf3d8c2f12a512
TemirB <balinov.tm21@physics.msu.ru>
2025-09-17 21:29:30 +0300

    feat: Set intersection

---
+++ 11/11.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func contain(slice []int, target int) bool {
+	for _, v := range slice {
+		if v == target {
+			return true
+		}
+	}
+	return false
+}
+func main() {
+	A := []int{1, 2, 3}
+	B := []int{2, 3, 4}
+
+	result := []int{}
+	for _, t := range B {
+		if contain(A, t) {
+			result = append(result, t)
+		}
+	}
+
+	fmt.Println(result)
+}
