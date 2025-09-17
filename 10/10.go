317f17486bd7195566d8fe0549670c0dd1ec979b
TemirB <balinov.tm21@physics.msu.ru>
2025-09-17 21:27:04 +0300

    feat: implement temperature grouping by 10-degree ranges

---
+++ 10/10.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	slice := []float64{-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5}
+	myMap := make(map[int][]float64)
+
+	for _, val := range slice {
+		key := int(val) / 10 * 10
+		myMap[key] = append(myMap[key], val)
+	}
+
+	fmt.Println(myMap)
+}
