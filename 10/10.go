d7f4034ee090a94b2e625e06821f064fe8a46c43
TemirB <balinov.tm21@physics.msu.ru>
2025-09-17 21:30:18 +0300

    feat(10): Set intersection

--- 10/10.go
+++ 10/10.go
@@ -4,12 +4,12 @@ import "fmt"
 
 func main() {
 	slice := []float64{-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5}
-	myMap := make(map[int][]float64)
+	set := make(map[int][]float64)
 
 	for _, val := range slice {
 		key := int(val) / 10 * 10
-		myMap[key] = append(myMap[key], val)
+		set[key] = append(set[key], val)
 	}
 
-	fmt.Println(myMap)
+	fmt.Println(set)
 }
