1cfd20abfbd176ec56ee72c14fe7d4da6c7bf005
TemirB <balinov.tm21@physics.msu.ru>
2025-09-17 22:03:53 +0300

    feat(10): Temperature grouping

--- 10/10.go
+++ 10/10.go
@@ -4,12 +4,12 @@ import "fmt"
 
 func main() {
 	slice := []float64{-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5}
-	set := make(map[int][]float64)
+	group := make(map[int][]float64)
 
 	for _, val := range slice {
 		key := int(val) / 10 * 10
-		set[key] = append(set[key], val)
+		group[key] = append(group[key], val)
 	}
 
-	fmt.Println(set)
+	fmt.Println(group)
 }
