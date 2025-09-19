4981f88d0cb4cf873cd41e16f00ccde5d80ec95f
TemirB <balinov.tm21@physics.msu.ru>
2025-09-19 11:41:14 +0300

    feat(4): Termination with Ctrl+C

--- 4/4.go
+++ 4/4.go
@@ -19,6 +19,7 @@ func main() {
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, os.Interrupt, syscall.SIGINT)
 
+	// Запуск задач
 	wg.Add(nWorkers)
 	for i := range nWorkers {
 		go work(ctx, &wg, i)
