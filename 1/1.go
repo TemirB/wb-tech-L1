170545ad6375055a5600d830e92185f5d5bfa88a
TemirB <balinov.tm21@physics.msu.ru>
2025-09-19 11:44:50 +0300

    feat(1): Embedding structures

--- 1/1.go
+++ 1/1.go
@@ -2,9 +2,6 @@ package main
 
 import "fmt"
 
-// Дана структура Human (с произвольным набором полей и методов).
-// Реализовать встраивание методов в структуре Action от родительской структуры Human (аналог наследования).
-
 type Human struct {
 	FirstName  string
 	SecondName string
